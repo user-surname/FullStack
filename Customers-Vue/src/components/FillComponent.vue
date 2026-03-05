0878884e77c4179ab548c149856895c89451a066
user-surname <smetannikovviacheslav@elpuig.xeill.net>
2026-03-05 17:02:03 +0100

    commit

---
+++ Customers-Vue/src/components/FillComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+const userName = ref("Maria");
+const emit = defineEmits(['login']);
+
+const login = () => {
+    emit('login', userName.value);
+};
+
+</script>
+
+<template>
+    <div class="fill">
+        <slot name="top"></slot>
+        <h2>Componente fill</h2>
+        <p>Nom d'usuari: {{ userName }}</p>
+        <p><button @click="login">Login</button></p>
+        <slot name="bottom"></slot>
+    </div>
+</template>
+
+<style scoped>
+.fill {
+    border: 4px solid goldenrod;
+}
+</style>
--- Customers-Vue/src/views/Home.vue
+++ Customers-Vue/src/views/Home.vue
@@ -1,9 +1,20 @@
 <script setup>
+import FillComponent from '../components/FillComponent.vue';
 import HeaderComponent from '../components/HeaderComponent.vue';
 import { ref } from 'vue';
 const myId = "unAltreId";
 const myCondition = true;
 const myValue = ref("el meu nom");
+const longitud = ref(myValue.value.length);
+
+const fruites = ['Poma', 'Plàtan', 'Taronja', 'Maduixa'];
+
+const displayName = ref("");
+
+const setName = (name) => {
+    displayName.value = name;
+}
+
 </script>
 
 <template>
@@ -15,8 +26,26 @@ const myValue = ref("el meu nom");
     <h3 :class="{clar: myCondition, fosc: !myCondition}">Exemple v-bind 2</h3>
 
     <h3>{{ myValue }}</h3>
+    <h3>Longitud de myValue: {{ longitud }}</h3>
     <input type="text" v-model="myValue" />
 
+    <ul>
+        <li v-for="(fruit, index) in fruites" :key="fruit">
+            {{ fruit }} - Index: {{ index }}
+        </li>
+    </ul>
+
+
+    <h2>Hola {{ displayName }}</h2>
+    <FillComponent @login="setName">
+        <template v-slot:top>
+            Text al top
+        </template>
+                <template v-slot:bottom>
+            Text al bottom
+        </template>
+        <h3>Contingut del FillComponent</h3>
+    </FillComponent>
 </template>
 
 <style scoped>
